6a224d1a41cd1d347234c85a4c6b1e7cb66a4e88
thiagodemelopachecos@gmail.com <thiagodemelopachecos@gmail.com>
2022-05-28 13:21:09 -0300

    Alura Pic Pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app.routing.module';
+import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
+import { CardModule } from './shared/components/card/card.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+  PhotosModule,
+  AppRoutingModule,
+  ErrorsModule,
+  CardModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
